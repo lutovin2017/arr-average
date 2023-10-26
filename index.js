c7569a2c1a732cb9f1b20cba142157cb4861b899
Lutovin Sergey <Lutovin2017@gmail.com>
2023-10-27 00:12:35 +0300

    готовое решение

---
+++ index.js
@@ -0,0 +1,12 @@
+const calculateAverage = (arr) => {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return sum / arr.length;
+};
